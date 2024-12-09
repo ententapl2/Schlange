5afb8842139d57ba957691fb7928765b057fa09d
ententapl <ententapl@DOM.SCI.EDU.PL>
2024-12-09 14:04:38 +0100

    3. commit

--- scripts.js
+++ scripts.js
@@ -7,14 +7,29 @@ var ctx = canvas.getContext("2d");
 
 
 var snakeBody = [];
+
+var apple = {
+    x:0,
+    y:0
+}
+
 var velocity = {
     x:0,
     y:0
 }
 var snakeHead = {
-    x:0,
-    y:0
+    x:5,
+    y:5
 };
+
+function generateAppleCords() {
+
+    let x = Math.floor(Math.random() * rows);
+    let y = Math.floor(Math.random() * cols);
+
+    apple =  {x:x, y:y};
+
+}
 
 function refreshGame() {
 
@@ -27,11 +42,44 @@ function refreshGame() {
         }
     }
 
+    ctx.fillStyle = "red";
+    ctx.fillRect(apple.x*boxSize, apple.y*boxSize, boxSize, boxSize);
+
     ctx.fillStyle = "green";
     ctx.fillRect(snakeHead.x * boxSize, snakeHead.y * boxSize, boxSize, boxSize);
 
+    if ((snakeHead.x >= canvas.width/boxSize) || (snakeHead.y >= canvas.height/boxSize) || (snakeHead.x < 0) || (snakeHead.y < 0)) {
+        "..."
+    }
+
+    if ((snakeHead.x == apple.x) && (snakeHead.y == apple.y)) {
+        snakeBody.push([apple.x, apple.y]);
+        generateAppleCords();
+
+    }
+
+    for (let i=0; i<snakeBody.length; i++ ) {
+
+        ctx.fillStyle = "green";
+        ctx.fillRect(snakeBody[i][0] * boxSize, snakeBody[i][1]* boxSize, boxSize, boxSize);
+
+    }
+   
+    if (snakeBody.length > 0) {
+
+        for (let i = snakeBody.length - 1; i > 0; i--) {
+            snakeBody[i][0] = snakeBody[i - 1][0];
+            snakeBody[i][1] = snakeBody[i - 1][1];
+        }
+        snakeBody[0][0] = snakeHead.x;
+        snakeBody[0][1] = snakeHead.y;
+
+}
+
     snakeHead.x += velocity.x;
     snakeHead.y += velocity.y;
+
+
 
 }
 
@@ -40,11 +88,10 @@ function startGame() {
 
     canvas.width = rows * boxSize;
     canvas.height = cols * boxSize;
+    generateAppleCords();
     setInterval(refreshGame, 100);
 
 }
-
-
 
 window.onload = startGame;
 window.addEventListener("keydown", (a)=>{
@@ -53,6 +100,4 @@ window.addEventListener("keydown", (a)=>{
     else if (a.key == "ArrowUp" && velocity.y >= 0 ) {velocity.y = -1; velocity.x = 0;}
     else if (a.key == "ArrowDown" && velocity.y <= 0 ) {velocity.y = 1; velocity.x = 0;}
 
-    console.log(a.key);
-    
 });

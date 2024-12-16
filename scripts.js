cd0ea337ac442180155f034621e87eed7d1f4a1e
domin12 <domin12@DOM.SCI.EDU.PL>
2024-12-16 10:52:22 +0100

    added input delay so a snake cannot turn 180 degrees

--- scripts.js
+++ scripts.js
@@ -5,6 +5,8 @@ const cols = 13;
 const boxSize = 50;
 const appleSize = boxSize;
 
+let canInput = true;
+
 const canvas = document.getElementById("frame");
 var ctx = canvas.getContext("2d");
 var SCORE = 0;
@@ -23,21 +25,21 @@ death.src = "./assets/death.wav";
 
 
 
-var stillRefresh=false;
+var stillRefresh = false;
 var snakeBody = [];
 
 var apple = {
-    x:0,
-    y:0
+    x: 0,
+    y: 0
 }
 
 var velocity = {
-    x:0,
-    y:0
+    x: 0,
+    y: 0
 }
 var snakeHead = {
-    x:5,
-    y:5
+    x: 5,
+    y: 5
 };
 
 function pressTostartGame(a) {
@@ -47,7 +49,7 @@ function pressTostartGame(a) {
         document.querySelector('#gameFrame').style.filter = "blur(0px)";
         document.querySelector('#info').style.display = "none";
 
-        stillRefresh=true;
+        stillRefresh = true;
         refreshGame();
         window.removeEventListener('keydown', pressTostartGame);
 
@@ -58,15 +60,15 @@ function generateAppleCords() {
 
     let x = Math.floor(Math.random() * rows);
     let y = Math.floor(Math.random() * cols);
-    snakeBody.forEach((item)=>{if (Math.round(item[0]) == x && Math.round(item[1]) == y) {generateAppleCords()}})
-    apple =  {x:x, y:y};
+    snakeBody.forEach((item) => { if (Math.round(item[0]) == x && Math.round(item[1]) == y) { generateAppleCords() } })
+    apple = { x: x, y: y };
 
 }
 
 function endGame() {
 
-    stillRefresh=false;
-    if (Math.floor(Math.random()*3) == 2) boom.play();
+    stillRefresh = false;
+    if (Math.floor(Math.random() * 3) == 2) boom.play();
     else death.play();
     if (SCORE > BESTSCORE) localStorage.snake_best_score = SCORE;
 
@@ -75,19 +77,20 @@ function endGame() {
 }
 
 async function refreshGame() {
+    canInput = true;
 
     vx = velocity.x;
     vy = velocity.y;
 
-    for (let n=0; n<segment; n++) {
+    for (let n = 0; n < segment; n++) {
 
-        ctx.shadowBlur=0;
-        ctx.clearRect(0,0, canvas.width, canvas.height);
+        ctx.shadowBlur = 0;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
         for (let x = 0; x < rows; x++) {
             for (let y = 0; y < cols; y++) {
-                
-                ctx.fillStyle = ((x-y)% 2 == 0) ? "#8ecc39" : "#a7d948";
+
+                ctx.fillStyle = ((x - y) % 2 == 0) ? "#8ecc39" : "#a7d948";
                 ctx.fillRect(x * boxSize, y * boxSize, boxSize, boxSize);
             }
         }
@@ -95,61 +98,61 @@ async function refreshGame() {
 
         ctx.fillStyle = "yellow";
         ctx.fillRect(snakeHead.x * boxSize, snakeHead.y * boxSize, boxSize, boxSize);
-        t = {x:Math.round(snakeHead.x), y:Math.round(snakeHead.y)};
+        t = { x: Math.round(snakeHead.x), y: Math.round(snakeHead.y) };
 
-        if ((t.x>= canvas.width/boxSize) || (t.y >= canvas.height/boxSize) || (t.x < 0) || (t.y < 0)) {endGame();return null;}
+        if ((t.x >= canvas.width / boxSize) || (t.y >= canvas.height / boxSize) || (t.x < 0) || (t.y < 0)) { endGame(); return null; }
         if ((Math.round(snakeHead.x) == apple.x) && (Math.round(snakeHead.y) == apple.y)) {
 
-            for (let q=0; q<4; q++) snakeBody.push([apple.x, apple.y]);
+            for (let q = 0; q < 4; q++) snakeBody.push([apple.x, apple.y]);
             sound.play();
             generateAppleCords();
             SCORE++;
 
         }
 
-        for (let i=0; i<snakeBody.length; i++ ) {
+        for (let i = 0; i < snakeBody.length; i++) {
 
             ctx.fillStyle = "green";
-            ctx.fillRect(snakeBody[i][0] * boxSize, snakeBody[i][1]* boxSize, boxSize, boxSize);
+            ctx.fillRect(snakeBody[i][0] * boxSize, snakeBody[i][1] * boxSize, boxSize, boxSize);
 
         }
 
         ctx.fillStyle = "red";
         img = new Image();
         img.src = "assets/apple.svg";
-        ctx.drawImage(img, apple.x*boxSize, apple.y*boxSize, appleSize, appleSize);
-    
+        ctx.drawImage(img, apple.x * boxSize, apple.y * boxSize, appleSize, appleSize);
+
         if (snakeBody.length > 0) {
 
             for (let i = snakeBody.length - 1; i > 0; i--) {
-                if ((snakeHead.x == snakeBody[i][0] && snakeHead.y == snakeBody[i][1])) {endGame();return null;}
+                if ((snakeHead.x == snakeBody[i][0] && snakeHead.y == snakeBody[i][1])) { endGame(); return null; }
 
                 let p1 = snakeBody[i - 1][0];
                 let p2 = snakeBody[i - 1][1];
 
                 snakeBody[i][0] = p1;
-                snakeBody[i][1] = p2;                
+                snakeBody[i][1] = p2;
 
-                
+
             }
             snakeBody[0][0] = snakeHead.x;
             snakeBody[0][1] = snakeHead.y;
 
         }
 
-        snakeHead.x += vx*(1/segment);
-        snakeHead.y += vy*(1/segment);
+        snakeHead.x += vx * (1 / segment);
+        snakeHead.y += vy * (1 / segment);
 
-        ctx.shadowColor="black";
-        ctx.shadowBlur=13;
-        ctx.lineWidth=3;
+        ctx.shadowColor = "black";
+        ctx.shadowBlur = 13;
+        ctx.lineWidth = 3;
         ctx.fillStyle = "white";
         ctx.font = "24px Courier New";
 
-        ctx.strokeText(("SCORE: " + SCORE),10,30);
-        ctx.fillText(("SCORE: " + SCORE),10,30);
-        ctx.strokeText(("BEST SCORE: " + BESTSCORE ),10,70);
-        ctx.fillText(("BEST SCORE: " + BESTSCORE ),10,70);
+        ctx.strokeText(("SCORE: " + SCORE), 10, 30);
+        ctx.fillText(("SCORE: " + SCORE), 10, 30);
+        ctx.strokeText(("BEST SCORE: " + BESTSCORE), 10, 70);
+        ctx.fillText(("BEST SCORE: " + BESTSCORE), 10, 70);
 
         await new Promise(r => setTimeout(r, speed));
     }
@@ -171,13 +174,15 @@ function startGame() {
 }
 
 window.onload = startGame;
-window.addEventListener("keydown", async function(a){
+window.addEventListener("keydown", async function (a) {
+    if(!canInput) return;
 
-    if (a.key == "ArrowRight" && velocity.x >= 0 ) {velocity.x = 1; velocity.y = 0;}
-    else if (a.key == "ArrowLeft" && velocity.x <= 0 ) {velocity.x = -1; velocity.y = 0;}
-    else if (a.key == "ArrowUp" && velocity.y <= 0 ) {velocity.y = -1; velocity.x = 0;}
-    else if (a.key == "ArrowDown" && velocity.y >= 0 ) {velocity.y = 1; velocity.x = 0;}
+    if (a.key == "ArrowRight" && velocity.x >= 0) { velocity.x = 1; velocity.y = 0; }
+    else if (a.key == "ArrowLeft" && velocity.x <= 0) { velocity.x = -1; velocity.y = 0; }
+    else if (a.key == "ArrowUp" && velocity.y <= 0) { velocity.y = -1; velocity.x = 0; }
+    else if (a.key == "ArrowDown" && velocity.y >= 0) { velocity.y = 1; velocity.x = 0; }
 
+    canInput = false;
 });
 
 window.addEventListener("keydown", pressTostartGame)
